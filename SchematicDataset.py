2ed365f41782e1971eaae4d010787d06b308e615
hmhornung <85147335+hmhornung@users.noreply.github.com>
2024-02-01 11:51:19 -0600

    started extracting 6x5x5 datapieces from schematics

--- SchematicDataset.py
+++ SchematicDataset.py
@@ -9,44 +9,42 @@ def create_dataset(schem_arrays: list[SchematicData],
                    data_shape: tuple,
                    dest: str,
                    dtype: np.dtype) -> int:
-    data_names = []
-    empty = 0
+    filenames = []
+    extracted = []
+    empty = []
+
     for i, schem_data in enumerate(schem_arrays):
+        if schem_data.blocks.shape[0] < data_shape[0] or schem_data.blocks.shape[1] < data_shape[1] or schem_data.blocks.shape[2] < data_shape[2]:
+            continue
         slice_result = slice_ndarray(schem_data, data_shape, dest + 'data/', dtype)
-        data_names = data_names + slice_result[0]
-        empty += slice_result[1]
-        print(f'extracted data from {schem_data.filename}')
+        filenames.append(schem_data.filename)
+        empty.append(slice_result[1])
+        extracted.append(slice_result[0])
+        print(f'extracted data from {schem_data.filename} with {slice_result[1]} empty slices | {slice_result[0]} extracted')
     
-    df_dict = {'data_filenames': data_names}
+    df_dict = {'filenames': filenames,
+               'extracted': extracted,
+               'empty': empty}
     df = pd.DataFrame(df_dict)
     df.to_csv(dest + 'data.csv')
     return empty
 
-def slice_ndarray(schem_data: SchematicData, shape: tuple, dest: str, dtype: np.dtype) -> (list[str], int):
+def slice_ndarray(schem_data: SchematicData, shape: tuple, dest: str, dtype: np.dtype) -> (int, int):
     filenames = []
     blocks = schem_data.blocks
     basename = schem_data.tkn_filename.removesuffix('.bin')
-    
-    slices = sliding_window_view(blocks, shape)
-    i = 0
     empty = 0
-    for slice_ in slices:
-        if len(np.unique(slice_)) == 1:
-            empty += 1
-        else:
-            name = basename + f'_{i}.bin'
-            filenames.append(name)
-            schemfio.write_ndarray(slice_, dest, name)
-            i += 1
-    # for x in range(schem_data.blocks.shape[0] - shape[0] + 1):
-    #     for y in range(schem_data.blocks.shape[1] - shape[1] + 1):
-    #         for z in range(schem_data.blocks.shape[2] - shape[2] + 1):
-    #             slice = np.empty(shape, dtype=dtype)
-    #             for i in range(shape[0]):
-    #                 for j in range(shape[1]):
-    #                     for k in range(shape[2]):
-    #                         slice[i,j,k] = blocks[ x+i , y+j , z+k ]
-    #             name = basename + f'_{x}_{y}_{z}.bin'
-    #             filenames.append(name)
-    #             schemfio.write_ndarray(slice, dest, name)
-    return filenames
+    extracted = 0
+    
+    for x in range(schem_data.blocks.shape[0] - shape[0] + 1):
+        for y in range(schem_data.blocks.shape[1] - shape[1] + 1):
+            for z in range(schem_data.blocks.shape[2] - shape[2] + 1):
+                slice = np.array(blocks[x:x+shape[0],y:y+shape[1],z:z+shape[2]], dtype=dtype)
+                if len(np.unique(slice)) == 1:
+                    empty += 1
+                    continue
+                name = basename + f'_{x}_{y}_{z}.bin'
+                filenames.append(name)
+                schemfio.save_ndarray(slice, dest + name)
+                extracted += 1
+    return extracted, empty
